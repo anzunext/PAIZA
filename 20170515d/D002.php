53317448a1dfbc428a911d75ab7e23cf98efc611
anzunext <kamimura.anzu@gmail.com>
2017-05-15 16:52:31 +0900

    0515d 002onlywritenow

---
+++ 20170515d/D002.php
@@ -0,0 +1,10 @@
+<?php
+//1
+$num = 1000
+ for ( $i = 0; $i > 1001; $i++ );
+
+echo $a < $b;
+echo $a > $b;
+
+echo $a = $b = "eq";
+?>
---
+++ 20170515d/D056.php
@@ -0,0 +1,31 @@
+<?php
+//1
+/*$r1 = 5;
+$r2 = 2;
+$r = $r1 ^ 3 - $r2 ^ 3;
+
+echo $r; //- r2^3
+*/
+ ?>
+<?php
+//1
+$input_lines = trim ( fgets ( STDIN ) );
+for ( $i = 0; $i < $input_lines; $i++ ) {
+    $r_1 = 5 ** 3;
+    $r_2 = 2 ** 3;
+    echo $m_3 = $r_1 - $r_2;
+    }
+?>
+
+
+<?php
+    // 自分の得意な言語で
+    // Let's チャレンジ！！
+    $input_lines = fgets(STDIN);
+    echo "input_lines";
+//1
+    $n = 0;
+    $a = $n + 3;
+    $b = $n - 4;
+    echo $a + $b;
+?>
