d2145fc0243c6b17bbf483861e26a1284f9d5321
anzunext <kamimura.anzu@gmail.com>
2017-05-16 08:57:38 +0900

    20170516commit

--- 20170515d/D002.php
+++ 20170515d/D002.php
@@ -1,11 +1,17 @@
 <?php
 //1
-$num = 1000
- for ( $i = 0; $i > 1001; $i++ );
-
-echo $a < $b;
-echo $a > $b;
-
-echo $a = $b = "eq";
+<?php
+    // 自分の得意な言語で
+    // Let's チャレンジ！！
+    $input_lines = fgets(STDIN);
+    $a = explode ( " " , $input_lines );
+    if ( $a[0] == $a[1] ){
+        echo "eq";
+    }
+    elseif ( $a[0] > $a[1] ){
+        echo $a[0];
+    }
+    else {
+        echo $a[1];
+    }
 ?>
-kkkkkkkkkk
